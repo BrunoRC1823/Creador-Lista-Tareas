fb4b37f9b567f3e85d0b4cc3c40fbc8c08ac7aae
Bruno Daniel Rios Cosser <bruno_rioscosser18_2314@hotmail.com>
2023-03-02 04:44:17 -0500

    Clase04 completada

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-const btn = document.querySelector("[data-form-btn]")
+(() => {
+    const btn = document.querySelector("[data-form-btn]")
 
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const valor = input.value;
-    const lista = document.querySelector("[data-list]");
-    const tarea = document.createElement('li');
-    tarea.classList.add('card')
-    input.value = '';
-    const contenido = `
-    <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${valor}</span>
-    </div>
+    const createTask = (evento) => {
+        evento.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        const lista = document.querySelector("[data-list]");
+        const tarea = document.createElement('li');
+        const contenidoTarea = document.createElement('div');
+        const tituloTarea = document.createElement('span');
+        const valor = input.value;
+        const contenido = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    tarea.innerHTML = contenido;
-    lista.appendChild(tarea);
-};
 
-btn.addEventListener("click", createTask)
+        input.value = '';
+        tarea.classList.add('card');
+        tituloTarea.classList.add('task');
+        tituloTarea.innerText = valor;
+
+        contenidoTarea.appendChild(checkComplete());
+        contenidoTarea.appendChild(tituloTarea);
+
+        tarea.appendChild(contenidoTarea);
+        lista.appendChild(tarea);
+    };
+
+    btn.addEventListener("click", createTask)
+    const checkComplete = () => {
+        const i = document.createElement("i");
+        i.classList.add('far', 'fa-check-square', 'icon');
+        i.addEventListener("click", completarTarea);
+        return i;
+    };
+    const completarTarea = (evento) => {
+        const elemento = evento.target
+        elemento.classList.toggle('fas');
+        elemento.classList.toggle('completeIcon');
+        elemento.classList.toggle('far');
+    };
+})();
